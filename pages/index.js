07d7248f84b1b86c3224b36b0172a240dd6fe26c
Wendel Santos <wendel.santos@pdcase.com.br>
2021-04-15 20:49:19 -0300

    teste prod

--- pages/index.js
+++ pages/index.js
@@ -3,26 +3,10 @@ import { useState } from "react";
 function Home() {
     return (
         <div>
-            <h1>Home - Teste</h1>
-            <Contador />
+            <h1>Home - Teste Prod</h1>
         </div>
     )
 }
 
-function Contador() {
-    const [contador, setcontador] = useState(1);
-
-    function adicionarContador(){
-        setcontador(contador + 1);
-    }
-
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Contar</button>
-        </div>
-    )
-
-}
 
 export default Home;
